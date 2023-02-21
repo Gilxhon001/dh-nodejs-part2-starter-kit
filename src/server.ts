d5c6e6e0f12a4b01fb0ae4bce50d76c01464ac50
Gilxhon <gilxhonhima@gmail.com>
2023-02-21 14:10:09 +0100

    Exercise 1 Finished

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,38 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
-console.log(TODO);
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets = [
+  { id: 1, name: "Earth" },
+  { id: 2, name: "Mars" },
+];
+
+app.get("/api/planets", function(req, res) {
+  res.status(200).json(planets);
+});
+
+app.get("/api/planets/:id", function(req, res) {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json(planet);
+});
+
+app.listen(port, () =>
+  console.log(`Example App Listening on port http://localhost:${port}`)
+);
