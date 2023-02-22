9bc50f7a772332ad7056dcbc830bde3e5521fa21
Gilxhon <gilxhonhima@gmail.com>
2023-02-22 17:00:57 +0100

    createImage Added

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -13,7 +13,8 @@ const setupDb = async () => {
 
   CREATE TABLE planets(
     id SERIAL NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    image TEXT
   );
   `);
 
@@ -97,4 +98,19 @@ const deleteById = async (req: Request, res: Response) => {
   res.status(200).json({ msg: "Planet was deleted" });
 };
 
-export { getAll, getOneById, create, updateById, deleteById };
+// POST /planets/:id/image
+const createImage = async (req: Request, res: Response) => {
+  console.log(req.file);
+
+  const {id} = req.params;
+  const fileName = req.file?.path;
+
+  if (fileName) {
+    db.none(`UPDATE planets SET image=$2 WHERE id=$1`, [id, fileName]) ;
+    res.status(201).json({msg: "Image Uploaded"})
+  }
+
+  res.status(400).json({msg: "Planet Image Failed to Upload"})
+}
+
+export { getAll, getOneById, create, updateById, deleteById, createImage };
