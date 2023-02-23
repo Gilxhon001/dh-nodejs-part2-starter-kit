6420639c30e1edcb0e3c93d44b17369b0aa902c2
Gilxhon <gilxhonhima@gmail.com>
2023-02-23 11:34:20 +0100

    users signup finished

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -11,20 +11,38 @@ const logIn = async (req: Request, res: Response) => {
 
   if (user && user.password === password) {
     const payload = {
-        id: user.id,
-        username
+      id: user.id,
+      username,
     };
-    const {SECRET = ""} = process.env;
-    const token = jwt.sign(payload, SECRET) ;
+    const { SECRET = "" } = process.env;
+    const token = jwt.sign(payload, SECRET);
 
     console.log(token);
 
+    await db.none(`UPDATE users SET token=$2 WHERE id=$1`, [user.id, token]);
 
-    await db.none(`UPDATE users SET token=$2 WHERE id=$1`, [user.id, token])
-
-    res.status(400).json({id: user.id, username, token});
+    res.status(200).json({ id: user.id, username, token });
   }
   res.status(400).json({ msg: "Username or password incorrect" });
 };
 
-export { logIn };
+const signUp = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await db.oneOrNone(
+    `SELECT ALL FROM users WHERE username=$1`,
+    username
+  );
+
+  if (user) {
+    res.status(409).json({ msg: "Username already in use" });
+  } else {
+    const { id } = await db.one(
+      `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id`,
+      [username, password]
+    );
+
+    res.status(201).json({ id, msg: "User created successfully" });
+  }
+};
+
+export { logIn, signUp };
